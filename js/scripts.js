d7cdf91014d942c4e8d88c51f1f38446cdaad3a8
Luch2307 <lucianoierace@hotmail.com>
2022-07-05 16:35:58 -0300

    u

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+let intersectFadeIn = () => {
+  const sections = document.querySelectorAll(".index-product-img");
+  const secArr = Array.from(sections);
+
+  const options = {
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+   
+
+    entries.map((entry) => {
+      if (entry.isIntersecting) {
+            console.log(entry.target);
+            entry.target.classList.add("active");
+       
+        observer.unobserve(entry.target);
+      }
+      return;
+    });
+  }, options);
+
+  secArr.map((section) => {
+    observer.observe(section);
+  });
+};
+
+window.addEventListener("load", intersectFadeIn, false);
+
