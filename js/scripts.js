34740d7fa8e41fa4bdcbcbbcf6ec7f70303c78b1
Luch2307 <lucianoierace@hotmail.com>
2022-07-21 18:56:06 -0300

    c

--- js/scripts.js
+++ js/scripts.js
@@ -25,5 +25,7 @@ let intersectFadeIn = () => {
   });
 };
 
-window.addEventListener("load", intersectFadeIn, false);
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+toggleButton.addEventListener('click',() =>{navbarLinks.classList.toggle('active')})
