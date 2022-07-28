1408bb5e985771f1fa5584b2fbb74979b36cb393
Luch2307 <lucianoierace@hotmail.com>
2022-07-28 18:11:22 -0300

    c

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,17 @@
-let intersectFadeIn = () => {
-  const sections = document.querySelectorAll(".index-product-img");
+
+  const sections = document.querySelectorAll(".product-info");
   const secArr = Array.from(sections);
 
   const options = {
-    threshold: 0.5,
+    threshold: 1,
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
-   
 
     entries.map((entry) => {
       if (entry.isIntersecting) {
             console.log(entry.target);
             entry.target.classList.add("active");
-       
         observer.unobserve(entry.target);
       }
       return;
@@ -21,11 +19,12 @@ let intersectFadeIn = () => {
   }, options);
 
   secArr.map((section) => {
-    observer.observe(section);
-  });
-};
+    observer.observe(section);                                                                
+  })
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click',() =>{navbarLinks.classList.toggle('active')})
+
+
